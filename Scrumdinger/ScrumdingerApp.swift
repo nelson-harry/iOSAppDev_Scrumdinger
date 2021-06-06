ec46d2399aff7e602478fddbec6edeabf1ca4fc0
nelsonharry <nelsonhy@yahoo.com>
2021-06-06 23:52:29 +0530

    Stack Views

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by NELSON HARRY on 06/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
